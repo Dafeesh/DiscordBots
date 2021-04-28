4c074db12ebc49dc1551b6c6ce81ae810056c2ff
Blake Scherschel <blake.scherschel@gmail.com>
2021-04-28 18:33:05 -0500

    More comments

--- bots/config.ts
+++ bots/config.ts
@@ -5,13 +5,13 @@ interface BotConfig {
     "RANDO_BOT_KEY": string
 };
 
+// Load sensitive Discord keys from ~/.creds
 const targetConfigFilePath = `/home/${process.env.USER}/.creds/discord-bots-keys.json`
 if (!fs.existsSync(targetConfigFilePath)) {
     throw (
         `Failed to find file: ${targetConfigFilePath}\n` + 
         "This is normal if you are attempting to run immediately after cloning.\n" +
         `Create a file: ${targetConfigFilePath}\n`
-        
     );
 }
 const config: BotConfig = JSON.parse(fs.readFileSync(targetConfigFilePath).toString('utf-8'));
